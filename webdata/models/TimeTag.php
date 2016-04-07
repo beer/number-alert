c6e8729e9cd9e6d343796f82fe7578f4aa13a4c3
Ronny Wang <ronnywang@gmail.com>
2016-04-07 18:18:04 +0800

    加上 TimeTag ，處理每個時間點的特殊事件

---
+++ webdata/models/TimeTag.php
@@ -0,0 +1,34 @@
+<?php
+
+class TimeTag extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'time_tag';
+        $this->_primary = array('tag_id');
+
+        $this->_columns['tag_id'] = array('type' => 'int', 'auto_increment' => true);
+        // 1 ymd
+        $this->_columns['group'] = array('type' => 'int');
+        $this->_columns['time'] = array('type' => 'int');
+        $this->_columns['tag'] = array('type' => 'varchar', 'size' => 255);
+
+        $this->addIndex('group_time', array('group', 'time'));
+    }
+
+    public function findTagsByTimes($group, $times)
+    {
+        $ret = array();
+        if (!$times) {
+            return $ret;
+        }
+        $records = TimeTag::search(array('group' => intval($group)))->searchIn('time', $times);
+        foreach ($records as $record) {
+            if (!array_key_exists($record->time, $ret)) {
+                $ret[$record->time] = array();
+            }
+            $ret[$record->time][] = $record->tag;
+        }
+        return $ret;
+    }
+}
