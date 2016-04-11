0051d83b1b0cd1bce0c79aafc6382a81574bbe0f
Ronny Wang <ronnywang@gmail.com>
2016-04-11 12:26:59 +0800

    修正抓錯 ID 問題

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -12,12 +12,12 @@ class IndexController extends Pix_Controller
 
     public function clusterAction()
     {
-        if (!$_GET['set']) {
-            $_GET['set'] = 'ptt:Japan_Travel';
+        if (!$_GET['id']) {
+            $_GET['id'] = 'ptt:Japan_Travel';
         }
         $k_value = intval($_GET['k']) ?: 10;
-        if (!$set = NumberSet::getSet(strval($_GET['set']), false)) {
-            return $this->json("找不到 {$_GET['set']}");
+        if (!$set = NumberSet::getSet(strval($_GET['id']), false)) {
+            return $this->json("找不到 {$_GET['id']}");
         }
         $this->view->set = $set;
         $this->view->cluster_set = PatternChecker::getClusteredPatterns($set, $k_value);
